c07791d9ee22abd870b7ecc9afa3e2add1a9b15d
stoyin <stoyin209@gmail.com>
2022-11-28 02:02:06 +0100

    database

--- py_create_database.py
+++ py_create_database.py
@@ -1,19 +1,14 @@
-import pymysql
-#establishing the connection
-conn =  pymysql.connect(user='root', password='******', host='127.0.0.1')#use your own password
-#Creating a cursor object using the cursor() method
-cursor = conn.cursor()
-#Doping database MYDATABASE if already exists.
-cursor.execute("DROP database IF EXISTS customers")
-#Preparing query to create a database
-sql = "CREATE database customers";
+import pymysql #establishing the connection
+conn =  pymysql.connect(user='root', password='******', host='127.0.0.1')#Creating a cursor object using the cursor() method
 
-#Creating a database
-cursor.execute(sql)
-#Retrieving the list of databases
+cursor = conn.cursor()#Doping database MYDATABASE if already exists.
+
+cursor.execute("DROP database IF EXISTS customers")#Preparing query to create a database
+sql = "CREATE database customers";
+cursor.execute(sql)#Creating a database
+
 print("List of databases: ")
-cursor.execute("SHOW DATABASES")
+cursor.execute("SHOW DATABASES")#Retrieving the list of databases
 print(cursor.fetchall())
 
-#Closing the connection
-conn.close()
+conn.close()#Closing the connection
